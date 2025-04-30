0c4cfa8b76f2f8baa4464eb7eeee029e9e63f572
floorba11 <maradan.luc.24@gmail.com>
2025-04-30 13:39:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="Team Schedule & Live Ticker", page_icon="🏒", l
 TEAMS = {
     "Tigers Langnau LUPL": 429523,
     "Herren Frutigen": 429611,
-    "Jets Damen": 431166,
+    "Regio Entlebuch": 432526,
 }
 
 def get_team_logo(team_name):
